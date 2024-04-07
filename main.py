9e2c818f4b4227b22724d77d5bae292098bea992
Milel88 <166305929+Milel88@users.noreply.github.com>
2024-04-07 16:26:14 +0300

    structure

--- main.py
+++ main.py
@@ -3,3 +3,41 @@
 # файл с главным кодом
 #
 #####################
+
+import cv2 as cv
+import numpy as np
+from picture_editing import VideoEditor
+
+cap=cv.VideoCapture("Traffic IP Camera video (1).mp4")
+
+bg_subs = cv.createBackgroundSubtractorMOG2()
+history = 300
+learning_rate = 1.0 / history
+
+while cap.isOpened:
+    ret,frame=cap.read()
+
+    if ret!=True:
+        break
+    frame=VideoEditor.frame_edit(frame,0.9)
+
+    mask=bg_subs.apply(frame,learningRate=learning_rate)
+    mask=VideoEditor.mask_edit(mask,5,1)
+
+    out=frame.copy()
+
+    #result=cv.bitwise_and(out,out,mask=mask)
+    #result = cv.bitwise_and(out, out, mask=result)
+
+    #cv.imshow("res",mask)
+    VideoEditor().find_ctr(mask,out)
+    cv.imshow("input", out)
+
+
+    #cv.imshow("input",frame)
+
+    c=cv.waitKey(10)
+    if c==27:
+        break
+cap.release()
+cv.destroyAllWindows()
--- util/picture_edit.py
+++ util/picture_edit.py
@@ -3,3 +3,26 @@
 # файл с методом для работы с изображениями
 #
 #####################
+import cv2 as cv
+import numpy as np
+class VideoEditor:
+    @staticmethod
+    def frame_edit(frame,scale):
+        frame=cv.resize(frame,None ,fx=scale,fy=scale,interpolation=cv.INTER_AREA)
+        return frame
+
+    @staticmethod
+    def mask_edit(frame,kernel,iteration):
+        kernel=np.ones((kernel,kernel),np.uint8)
+        #dilation=cv.dilate(frame,kernel,iterations=iteration)
+        opening=cv.morphologyEx(frame,cv.MORPH_OPEN,kernel)
+        return opening
+
+
+    @staticmethod
+    def find_ctr(frame,res):
+        ret,thresh=cv.threshold(frame,170,255,0)
+        im2,contours,hierarchy=cv.findContours(
+            thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE
+        )
+        cv.drawContours(res,contours,-1,(0,255,0),3)
